b59fbf8859145c54dc09249cd0186da08710b3e6
mx-studio <bin_rob@mail.ru>
2022-06-25 20:33:23 +0700

    0.0.45

---
+++ update.cmd
@@ -0,0 +1,12 @@
+ECHO OFF
+for /F "tokens=1,2,3 delims=." %%G IN ('composer config version') do (
+    SET VER_MAJ=%%G
+    SET VER_MIN=%%H
+    SET VER_PATCH=%%I
+)
+SET /A VER_PATCH=VER_PATCH+1
+SET VER=%VER_MAJ%.%VER_MIN%.%VER_PATCH%
+REM composer config version %VER%
+git commit -a -m %VER%
+git tag %VER%
+echo "Version updated to %VER%"

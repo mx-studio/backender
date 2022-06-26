1db3382133217fa916afc6bfc00528550c285289
mx-studio <bin_rob@mail.ru>
2022-06-26 16:02:24 +0700

    0.0.78

--- core/ControllerCRUDTrait.php
+++ core/ControllerCRUDTrait.php
@@ -3,18 +3,31 @@ namespace adjai\backender\core;
 
 trait ControllerCRUDTrait {
 
-    public function actionItems() {
+    public function actionItems($where = [], $fields = '*', $count = null, $orderBy = [], $page = 1) {
+        $this->traitActionItems($where, $fields, $count, $orderBy, $page);
+    }
+
+    protected function traitActionItems($where = [], $fields = '*', $count = null, $orderBy = [], $page = 1) {
         $model = $this->getRelatedModel();
-        $this->outputData($model::getItems());
+        $numRows = $count === null ? null : [($page - 1) * $count, $count];
+        $this->outputData($model::getItems($where, $fields, $numRows, $orderBy));
     }
 
     public function actionRemove($id) {
+        $this->traitActionRemove($id);
+    }
+
+    protected function traitActionRemove($id) {
         $model = $this->getRelatedModel();
         $model::remove($id);
         $this->outputData();
     }
 
     public function actionUpdate($model) {
+        $this->traitActionUpdate($model);
+    }
+
+    protected function traitActionUpdate($model) {
         $model = $this->getRelatedModel();
         $model::update($model);
         $this->outputData();
--- core/ModelCRUDTrait.php
+++ core/ModelCRUDTrait.php
@@ -3,15 +3,27 @@ namespace adjai\backender\core;
 
 trait ModelCRUDTrait {
 
-    public static function getItems() {
-        return self::_get();
+    public static function getItems($where = [], $fields = '*', $numRows = null, $orderBy = []) {
+        return self::traitGetItems($where, $fields, $numRows, $orderBy);
+    }
+
+    protected static function traitGetItems($where = [], $fields = '*', $numRows = null, $orderBy = []) {
+        return self::_get($where, $fields, $numRows, $orderBy);
     }
 
     public static function remove($id) {
+        self::traitRemove($id);
+    }
+
+    protected static function traitRemove($id) {
         self::_remove(['id' => $id]);
     }
 
     public static function update($fields) {
+        return self::traitUpdate($fields);
+    }
+
+    protected static function traitUpdate($fields) {
         if (isset($fields['id'])) {
             $id = $fields['id'];
             unset($fields['id']);

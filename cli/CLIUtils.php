1a517834a98d45d92bcc06b004ade94104483b1a
Sergey <adjai@mail.ru>
2024-01-11 05:49:40 +0700

    0.0.109

--- cli/CLIUtils.php
+++ cli/CLIUtils.php
@@ -71,7 +71,7 @@ class CLIUtils {
                 copy($vendorDirectory . "/adjai/backender/samples/" . $sampleFile, $destinationFile);
                 if ($destinationFileName === 'config.php') {
                     self::fillFiles($destinationFile, [
-                        "define('BACKEND_BASE_URL', '');" => "define('BACKEND_BASE_URL', '$webappDirectory');",
+                        "define('SITE_BACKEND_URL', '');" => "define('SITE_BACKEND_URL', '$webappDirectory');",
                         "define('MODE', '');" => "define('MODE', '$appMode');",
                     ]);
                 } elseif ($destinationFileName === "config.$appMode.php") {

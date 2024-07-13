892a34451208db8490daf94c347897daf2ed3926
Sergey Kulikov <bin_rob@mail.ru>
2024-07-13 18:28:18 +0700

    0.0.139

--- controllers/JournalController.php
+++ controllers/JournalController.php
@@ -6,9 +6,9 @@ use adjai\backender\models\Journal;
 
 class JournalController extends Controller {
 
-    public function actionSave($juid, $action, $params = null) {
+    public function actionSave($juid, $action, $referer = null, $params = null) {
         $ip = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] : $_SERVER['REMOTE_ADDR'];
-        Journal::save($juid, $action, $params, $ip, $_SERVER['HTTP_USER_AGENT'] ?? '');
+        Journal::save($juid, $action, $referer, $params, $ip, $_SERVER['HTTP_USER_AGENT'] ?? '');
         $this->outputData();
     }
 
--- models/Journal.php
+++ models/Journal.php
@@ -5,11 +5,12 @@ use adjai\backender\core\DBModel;
 
 class Journal extends DBModel {
 
-    public static function save($juid, $action, $params, $ip, $userAgent) {
+    public static function save($juid, $action, $referer, $params, $ip, $userAgent) {
         self::_insert([
             'juid' => $juid,
             'datetime' => date('Y-m-d H:i:s'),
             'ip' => $ip,
+            'referer' => $referer,
             'useragent' => $userAgent,
             'action' => $action,
             'params' => $params === null ? null : json_encode($params),
